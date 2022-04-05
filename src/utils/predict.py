382f0b0bd13bd451ff2b2664d3250b93bce99273
c17hawke <sunny.c17hawke@gmail.com>
2022-04-05 16:10:29 +0530

    prediction module complete

--- src/utils/predict.py
+++ src/utils/predict.py
@@ -3,6 +3,7 @@ import imghdr
 import numpy as np
 import sys
 import cv2
+import tensorflow.lite as tflite
 
 def get_img(img_path):
     exist = os.path.join(img_path)
@@ -17,17 +18,36 @@ def get_img(img_path):
     
     return img
 
-def load_model():
-    pass
+def load_model(model_path):
+    interpreter = tflite.Interpreter(model_path)
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    return interpreter, input_details, output_details
 
-def resize_expand_img():
-    pass
+def resize_expand_img(input_img, input_details):
+    _, pixel_ht, pexel_wd, _ = input_details[0]['shape']
+    resized_img = cv2.resize(input_img, (pixel_ht, pexel_wd))
+    expand_img = np.expand_dims(resized_img, 0)
+    return expand_img
 
 def prediction(config):
     img_path = config["predict"]["test_img_path"]
     model_path = os.path.join(config["train"]["model_file_dir"], "model.tflite")
     label_map = config["predict"]["label_map"]
     input_img = get_img(img_path)
-    interpreter = load_model()
-    input_img = resize_expand_img()
+    interpreter, input_details, output_details = load_model(model_path)
+    input_img = resize_expand_img(input_img, input_details)
 
+    # prediction
+    input_index = input_details[0]['index']
+    interpreter.set_tensor(input_index, input_img)
+    interpreter.invoke()
+    output_index = output_details[0]['index']
+    output = interpreter.get_tensor(output_index)
+    pred =  np.squeeze(output)
+
+    argmax = np.argmax(pred)
+    result = label_map[argmax]
+    print(f"predicted: {result}")
+    return result, argmax
