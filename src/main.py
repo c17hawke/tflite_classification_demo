ca47bdeeb0f0cf30993ec8236bd600b58f0ee0a3
c17hawke <sunny.c17hawke@gmail.com>
2022-04-05 14:33:46 +0530

    predict module st 04

--- src/main.py
+++ src/main.py
@@ -3,11 +3,13 @@ import argparse
 from pathlib import Path
 from src.utils.common import read_yaml
 from src.utils.train import training_model
+from src.utils.predict import prediction
 
 def main(training, config):
     config = read_yaml(config)
     if not training:
         print("do prediction")
+        prediction(config)
     else:
         print("do training")
         training_model(config)
---
+++ src/utils/predict.py
@@ -0,0 +1,4 @@
+
+
+def prediction(config):
+    pass
